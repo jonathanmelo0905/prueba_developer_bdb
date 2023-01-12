18688c9e3f0adb9c0652d2487a3b8b9f562b9256
jonathanmelo0905 <98371133+jonathanmelo0905@users.noreply.github.com>
2023-01-12 15:25:50 -0500

    Update codeThree.ts

--- codeThree.ts
+++ codeThree.ts
@@ -1,53 +1,52 @@
-const coins : number[] = [1,1,1,1,1,1,1,1];
+const coins : number[] = [1,22,2,2,2,5,6,1,2,3,3,3,2,2,2,4,4,89,5,6,6,7,7,2,3,9,1,2,3,3,3,33,5,6,12,22,33,44,55,66];
+//const coins : number[] = [1,2,2,2,10,78,12,45,21]
 
-
-const coinsVueltas = (coin: any) => {
-    let coin1 = [].concat(coin);
-    coin1.sort((a, b)=> a-b)
-    let long = coin1.length;
-    if(long){
-        let suma = 0;
-        let coin2 = [];
-        let coin3 = [].concat(coin1.filter((e) => e >= 10)) 
-        coin1 = coin1.filter((e) => e<10) 
+const minimumMoney = (coin: any) => {
+    let long = coin.length;
+    if(long){        
+        coin.sort((a: number, b: number)=> a-b);
+        let coin1 = [].concat(coin.filter((e: number) => e < 10));
+        let coin3 = [].concat(coin.filter((e: number) => e >= 10));
         let band1 = 0;
-        let numref = 1; 
+        let numref = 1;
         while(band1 === 0){
-            let band2 =0 ;
-            let busqueda = coin1.find((e) => e === numref) 
-            if(busqueda === numref){
-                numref++
+
+            let band = 0;
+
+            while(band === 0){
+                let busqueda = coin1.find((e) => e === numref) 
+                if(busqueda === numref){
+                    numref++;
+                }else{
+                    band = 1;
+                }
             }
-            coin2 = [].concat(coin1.filter((e) => e <= numref))
-            let permutaciones = permutations(coin2)
-            let long2 = permutaciones.length;
-            for(let i = 0; i<long2; i++){
-                let long3 = permutaciones[i].length
-                suma = 0;
-                for(let j=0; j<long3; j++){
-                    suma += permutaciones[i][j];
+
+            let coin2 = [].concat(coin1.filter((e) => e <= numref))
+            let res = add(coin2);
+            if(res >= numref) numref = res;
+
+            if(numref >= 10){
+                band1 = 1;
+            }else{
+                let long2 = coin2.length
+                let suma = 0;
+                for(let i = 0; i<long2; i++){
+                    suma += coin2[i]
                     if(suma === numref){
-                        numref ++; 
-                        j = long3;
                         i = long2;
-                        band2 = 1;
+                        numref++;
                     }else if(suma > numref){
-                        j = long3;
+                        i = long2;
+                    }
+                    if(i === long2-1 && suma < numref){
+                        band1 = 1;
                     }
                 }
             }
-            if(numref >= 10){
-                band2 = 0;
-            }
-            if(band2 === 0 ){
-                band1 = 1;
-            }
         }
         if(numref >= 10){
-            numref = 1;
-            for(let num of coin1){
-                numref += num;
-            }
+            numref = 1 + add(coin1);
         }
         for(let num of coin3){
             if(numref >= num){
@@ -61,17 +60,8 @@ const coinsVueltas = (coin: any) => {
     }
 }
 
-const permutations: any = (arr: number[]) => {
-    if (arr.length <= 2) {
-        return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
-    }
+const add = (arr: number[]) => {
+    return arr.reduce((a, b) => a + b, 0);
+};
 
-    return arr.reduce((acc, el, i) =>
-        acc.concat(
-            permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map((val: number[]) => [el, ...val])
-        ),
-        []
-    );
-}
-
-coinsVueltas(coins)
+minimumMoney(coins)
